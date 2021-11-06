22f29c7d259be115ebf32cf54f2d74e65f7281d6
Andrei Avram <avramandreitiberiu@gmail.com>
2021-11-06 16:47:59 +0000

    Added Firebase lab testing

---
+++ scripts/run-robo-tests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
+
+gcloud firebase test android run \
+  --type robo \
+  --app "$BITRISE_APK_PATH" \
+  --device model=Pixel3,version=30,orientation=portrait \
+  --timeout 90s
